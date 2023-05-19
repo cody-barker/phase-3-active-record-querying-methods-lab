34939ccf8bb29132ab4c1ad187c5aa816584580e
Cody <codybarker.or@gmail.com>
2023-05-19 10:51:18 -0700

    s

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,16 +1,31 @@
 class Show < ActiveRecord::Base
 
     def self.highest_rating
-        self.maximum(:rating)
+       self.maximum(:rating)
     end
 
     def self.most_popular_show
-        #self.find_by(self.highest_rating)
-        
+        self.find_by(rating: self.highest_rating)
     end
 
     def self.lowest_rating
         self.minimum(:rating)
+    end
+
+    def self.least_popular_show
+        self.find_by(rating: self.lowest_rating)
+    end
+
+    def self.ratings_sum
+        self.where()
+    end
+
+    def self.popular_shows
+        
+    end
+
+    def self.shows_by_alphabetical_order
+   
     end
 
 end
